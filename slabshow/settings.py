313de78d1fea69bf89fc52ff187dcc3e74f429a8
gvilloslado <gvilloslado@lexico.ai>
2024-07-01 22:48:13 -0700

    running server

--- slabshow/settings.py
+++ slabshow/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "slabshow.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # Add this line
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -109,6 +109,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Media files settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- slabshow/urls.py
+++ slabshow/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def redirect_to_feed(request):
+    return redirect("feed")
+
 
 urlpatterns = [
+    path("", redirect_to_feed, name="home_page"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("profiles/", include("profiles.urls")),
@@ -9,3 +18,9 @@ urlpatterns = [
     path("feed/", include("feed.urls")),
     path("followers/", include("followers.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
